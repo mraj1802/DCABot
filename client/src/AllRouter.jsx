4087a99b91d62a0d7bb9d3bc97df35d145401f50
nlempiric <nl.empiric@gmail.com>
2024-01-02 11:50:16 +0530

    new changes

--- client/src/AllRouter.jsx
+++ client/src/AllRouter.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import SignIn from './Pages/SignIn'
+import CreateDcaBot from './Pages/CreateDcaBot'
 
 const AllRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/createbot" element={<CreateDcaBot/>}/>
     </Routes>
   
   )
