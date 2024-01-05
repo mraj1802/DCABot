329e0790830b14277ae395ec38e4b50a37b1d0e0
nlempiric <nl.empiric@gmail.com>
2024-01-05 11:55:41 +0530

    change header

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -4,17 +4,34 @@ import { Link, useNavigate } from "react-router-dom";
 import { IoLogOutSharp } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
+import { SiTether } from "react-icons/si";
+import { SiEthereum } from "react-icons/si";
+import { SiBitcoinsv } from "react-icons/si";
+
 const Header = () => {
-  const [isDropdown, setisDropdown] = useState(false);
-  const [isuserDropdown, setuserDropdown] = useState(false);
+  const [isDropdown, setIsDropdown] = useState(false);
+  const [isUserDropdown, setUserDropdown] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('USDT -Bal');
   const dropdownRef = useRef(null);
   const userdropdownRef = useRef(null);
   const navigate = useNavigate();
+
+  const coinData=[
+    {name:"BTC",icon:<SiBitcoinsv/>,bal:"12345"},
+    {name:"BTC",icon:<SiBitcoinsv/>,bal:"12345"},
+    {name:"BTC",icon:<SiBitcoinsv/>,bal:"12345"},
+  ]
+
+  const handleSelect = (value) => {
+    setSelectedValue(value);
+    setIsDropdown(false);
+  };
+
   const handleDropdown = () => {
-    setisDropdown(!isDropdown);
+    setIsDropdown(!isDropdown);
   };
   const handleuserDropdown = () => {
-    setuserDropdown(!isuserDropdown);
+    setUserDropdown(!isUserDropdown);
   };
 
   const handleLogout = () => {
@@ -25,7 +42,7 @@ const Header = () => {
 
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setisDropdown(false);
+      setIsDropdown(false);
     }
   };
   const handleClickOutsideuser = (event) => {
@@ -33,7 +50,7 @@ const Header = () => {
       userdropdownRef.current &&
       !userdropdownRef.current.contains(event.target)
     ) {
-      setuserDropdown(false);
+      setUserDropdown(false);
     }
   };
 
@@ -56,10 +73,10 @@ const Header = () => {
       <div className="flex  items-center gap-3">
         <div className="z-50 relative w-[150px]" ref={dropdownRef}>
           <button
-            className="w-full flex justify-between gap-1 items-center py-1.5 px-4 font-medium bg-blue-600 text-white rounded-md text-base"
+            className="w-full flex justify-between gap-1 items-center py-1.5 px-4 font-medium border border-gray-400 text-black rounded-md text-base"
             onClick={handleDropdown}
           >
-            USDT -Bal{" "}
+           {selectedValue}
             <span>
               {" "}
               <RxCaretDown />
@@ -70,10 +87,9 @@ const Header = () => {
             <>
               <div className="absolute bg-white z-50 top-12 right-0 left-0  p-2 py-3 px-2  border border-gray-200 shadow-xl rounded-md text-black font-normal">
                 <ul className="flex flex-col gap-3 cursor-pointer px-2">
-                  <li className="hover:text-blue-600">USDT -Bal</li>
-                  <li className="hover:text-blue-600">USDT -Bal</li>
-                  <li className="hover:text-blue-600">USDT -Bal</li>
-                  <li className="hover:text-blue-600">USDT -Bal</li>
+                  <li onClick={() => handleSelect('USDT -Bal')} className="flex items-center gap-2 hover:text-blue-600"><span className="text-green-600 text-lg"><SiTether/></span>USDT</li>
+                  <li onClick={() => handleSelect('BTC -Bal')} className="flex items-center gap-2 hover:text-blue-600"><span className="text-yellow-500 text-lg"><SiBitcoinsv/></span>BTC </li>
+                  <li onClick={() => handleSelect('ETH -Bal')} className="flex items-center gap-2 hover:text-blue-600"><span className="text-gray-800 text-lg"><SiEthereum/></span>ETH </li>
                 </ul>
               </div>
             </>
@@ -87,7 +103,7 @@ const Header = () => {
             />
           </div>
 
-          {isuserDropdown && (
+          {isUserDropdown && (
             <>
               <div className="absolute bg-white top-10 right-0 w-32 p-2 py-3 px-2  border border-gray-200 shadow-xl rounded-md text-black font-normal">
                 <ul className="flex flex-col gap-3 cursor-pointer px-2">
